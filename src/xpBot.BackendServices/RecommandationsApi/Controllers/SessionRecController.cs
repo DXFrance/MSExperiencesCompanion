0f2c811ac328e8e7c6f96c574bfb7e1e2104e6d8
Benjamin Talmard <benjamin@talmard.com>
2016-07-30 03:28:43 +0200

    Create Recommandation API controllers

---
+++ src/xpBot.BackendServices/RecommandationsApi/Controllers/SessionRecController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecommandationsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SessionRecController : Controller
+    {
+        // GET api/SessionRec/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
