eb43034faebf38450de4707b9653d6a34ffb7330
Benjamin Talmard <benjamin@talmard.com>
2016-07-31 01:07:54 +0200

    Implement the V1 of Rating API

--- src/xpBot.BackendServices/RatingApi/Controllers/RatingsController.cs
+++ src/xpBot.BackendServices/RatingApi/Controllers/RatingsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TextAnalytics;
 
 namespace RatingApi.Controllers
 {
@@ -36,7 +37,16 @@ namespace RatingApi.Controllers
         [Route("cs")]
         public async Task<IActionResult> Post([FromBody]Models.CSRating rating)
         {
-            // todo : benjat : rating.SentimentalText
+            string AccountKey = "a59b8894fe7a48ef8bef36865c8e561c";
+            string BaseUri = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0";
+
+            TextAnalyticsApiWrapper recommender = new TextAnalyticsApiWrapper(AccountKey, BaseUri);
+
+            int value = recommender.GetSentiment(rating.SentimentalText);
+
+            Models.Rating result = new Models.Rating() { UserId = rating.UserId, SessionId = rating.SessionId, Notation = value };
+
+            _dbContext.Ratings.Add(result);
             await _dbContext.SaveChangesAsync();
             return new CreatedResult(string.Empty, null);
         }
---
+++ src/xpBot.BackendServices/RatingApi/TextAnalyticsAPI/TextAnalyticsApiWrapper.cs
@@ -0,0 +1,125 @@
+﻿namespace TextAnalytics
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using System.Linq;
+    using System.Threading;
+    using System.Text;
+
+
+    /// <summary>
+    /// A wrapper class to invoke Recommendations REST APIs
+    /// </summary>
+    public class TextAnalyticsApiWrapper
+    {
+        private readonly HttpClient _httpClient;
+        public string BaseUri;
+
+        /// <summary>
+        /// Constructor that initializes the Http Client.
+        /// </summary>
+        /// <param name="accountKey">The account key</param>
+        public TextAnalyticsApiWrapper(string accountKey, string baseUri)
+        {
+            BaseUri = baseUri;
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUri),
+                Timeout = TimeSpan.FromMinutes(5),
+                DefaultRequestHeaders =
+                {
+                    {"Ocp-Apim-Subscription-Key", accountKey}
+                }
+            };
+        }
+
+        public int GetSentiment(string sentimentText)
+        {
+            string uri = BaseUri + "/sentiment";
+            //TODO : serialize an object and use JsonContent instead of StringContent
+            string text = @"{
+                           ""documents"": [
+                                    {
+                                        ""language"": ""en"",
+                                        ""id"": ""1"",
+                                        ""text"": """ + sentimentText + @"""
+                                    }
+                                ]
+                            }";
+
+            var response = _httpClient.PostAsync(uri, new StringContent(text, Encoding.UTF8, "application/json")).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(
+                    String.Format("Error {0}: Failed to get sentiment, Reason: {1}",
+                    response.StatusCode, ExtractErrorInfo(response)));
+            }
+
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            //TODO : desarialize object
+            //var round = Math.Round(result * 10, 0);
+
+            return 2;
+        }
+
+        /// <summary>
+        /// Extract error message from the httpResponse, (reason phrase + body)
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        private static string ExtractErrorInfo(HttpResponseMessage response)
+        {
+            string detailedReason = null;
+            if (response.Content != null)
+            {
+                detailedReason = response.Content.ReadAsStringAsync().Result;
+            }
+            var errorMsg = detailedReason == null ? response.ReasonPhrase : response.ReasonPhrase + "->" + detailedReason;
+            return errorMsg;
+
+        }
+
+        /// <summary>
+        /// Extract error information from HTTP response message.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        internal static string ExtractReponse(HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsStringAsync().Result;
+            }
+            string detailedReason = null;
+            if (response.Content != null)
+            {
+                detailedReason = response.Content.ReadAsStringAsync().Result;
+            }
+            var errorMsg = detailedReason == null ? response.ReasonPhrase : response.ReasonPhrase + "->" + detailedReason;
+
+            string error = String.Format("Status code: {0}\nDetail information: {1}", (int)response.StatusCode, errorMsg);
+            throw new Exception("Response: " + error);
+        }
+    }
+
+    /// <summary>
+    /// Utility class holding the result of import operation
+    /// </summary>
+    internal class ImportReport
+    {
+        public string Info { get; set; }
+        public int ErrorCount { get; set; }
+        public int LineCount { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("successfully imported {0}/{1} lines for {2}", LineCount - ErrorCount, LineCount,
+                Info);
+        }
+    }
+}
