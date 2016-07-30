da9502a6197e2cb17a3fca68887a9b8a91ee46ec
jdupuy <jdupuy@JDUPUY-DEVHOME>
2016-07-30 09:57:01 -0700

    Adding services integration (Inwink)

---
+++ src/xpBot/xpBot/InwinkLibrary/InwinkClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InwinkLibrary
+{
+    public static class InwinkClientFactory
+    {
+        /// <summary>
+        /// Create The Inwink Platform client
+        /// </summary>
+        public static IInwinkClient CreateInwinkClient()
+        {
+            return new InwinkClient();
+        }
+    }
+}
